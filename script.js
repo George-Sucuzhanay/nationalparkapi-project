ef74152970f7f096ea7dc0c805f1e8089e9f0ae5
George <georgesucuzhanay100@gmail.com>
2021-11-14 20:10:47 -0500

    accessing API and making calls to activites.name

--- script.js
+++ script.js
@@ -11,28 +11,35 @@
 //     $('#sunrise').text(data.results.sunrise);
 //     $('#sunset').text(data.results.sunset);
 // });
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('Page Loaded')
-})
 
-let button = document.getElementById("myBtn")
+let button = document.getElementById("myBtn").addEventListener('click', getText)
 
-
-button.onclick = () => {
-  console.log('button pressedww')
+function getText () {
   fetch("https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=Xl9ulMh0BRyo7Q7dX2vUBdl5uadPjptgOHpnKRL5")
-  .then(response => response.json())
-  .then(data => {
-    //console.log(data);
-
-    // For each Pokemon in the list
-    console.log(data.activities.name)
-    for (let acts of data.activities.name) {
-      //Create a div with the class .pokemon and append it to the #pokelist
-      console.log(data.activities.name);
-      $('#actlist').append(`<div class="col-4 pokemon">${acts.activities.name}</div>`);
-      
-    }
-});
-
+    .then(response => {
+      return (response.json())
+    })
+    .then(data => {
+      console.log(data.data[0].activities)
+    })
+    //   for (let cards of data.data[0].activities) {
+    //     //Create a div with the class .pokemon and append it to the #pokelist
+    //     console.log(cards.name);
+    //     $('actlist').append(`<div class="col-4 cards">${cards.name}</div>`);
+        
+    //   }
+    // })
+    .then(error => {
+      console.log(error)
+    })
+  //     // for (let acts of data.activities.name) {
+  //     //   //Create a div with the class .pokemon and append it to the #pokelist
+  //     //   console.log(data.activities.name[1]);
+  //     //   $('#actlist').append(`<div class="col-4 pokemon">${acts.activities.name[1]}</div>`);
+        
+  //     // }
 }
+
+getText()
+
+  
